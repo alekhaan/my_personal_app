bb0412bc22030833f314e090ae9a4034acfaceff
Alex God <amborkhodoev@edu.hse.ru>
2023-10-08 13:55:00 +0300

    Initial Commit

---
+++ my_personal_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my_personal_app
+//
+//  Created by AlexGod on 08.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

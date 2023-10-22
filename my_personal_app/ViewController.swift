6e2c868707e5f5ca939b47266dedebb0760dc2c7
Alex God <amborkhodoev@edu.hse.ru>
2023-10-22 15:09:48 +0300

    added rick and morty api

--- my_personal_app/ViewController.swift
+++ my_personal_app/ViewController.swift
@@ -12,7 +12,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .orange
+        
+        var url: URL = URL(string: "https://rickandmortyapi.com/api/character")!
+        URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+            guard
+                let data,
+                error == nil
+            else { return }
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            var model = try! decoder.decode(CharactersDTO.self, from: data)
+            print(model)
+        }).resume()
+    
     }
     
 }
 
+struct CharactersDTO: Decodable {
+    struct CharacterDTO: Decodable {
+        let name: String
+        let status: String
+        let species: String
+        let image: URL
+    }
+    
+    struct InformationDTO: Decodable {
+        let count: Int
+        let pages: Int
+        let next: URL?
+        let prev: URL?
+    }
+    
+    let info: InformationDTO
+    let results: [CharacterDTO]
+}
